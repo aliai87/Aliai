54b5f6376b8b9711b0fa4ff94299167e1d60f3e1
aliai87 <Alizaman7446@gmail.com>
2026-05-29 22:54:02 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import google.generativeai as genai
 
-# اپنی Google AI Studio API Key یہاں ڈالیں
-genai.configure(api_key="import os
-genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])")
+# Streamlit Secrets سے API Key حاصل کرنا
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 st.title("AliAI - Your Intelligent Companion")
 user_input = st.text_input("Ask your question:")
